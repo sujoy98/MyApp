1dd45b71b2e118199604918de4b39931c3e2d986
Sujoy Sarkar <sujoysarkar951@gmail.com>
2020-08-12 23:03:23 +0530

    update

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -41,7 +41,7 @@ class PostsController extends Controller
      */
     public function create()
     {
-        //
+        return view('posts/create');
     }
 
     /**
@@ -52,7 +52,11 @@ class PostsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request,[
+            'title'=> 'required',
+            'body'=>'required'
+        ]);
+        return 123;
     }
 
     /**
